9643def484990f83f65c1a9444fc77cb4e9092c3
Vladas <baradulinasvladas@gmail.com>
2023-07-26 14:10:38 +0300

    reverted back

--- app/Components/fetchDatabaseItems.tsx
+++ app/Components/fetchDatabaseItems.tsx
@@ -1,23 +1,8 @@
 import AnotherContainer from "./anotherContainer";
 
-async function getData() {
-  const res = await fetch("srv964.hstgr.io");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-export default async function FetchDatabaseItems() {
+export default function FetchDatabaseItems() {
   //receives info through props for database connection
   //those items are passed as a prameter object to the another container
-  const data = await getData();
-  console.log(data);
+
   return <AnotherContainer></AnotherContainer>;
 }
