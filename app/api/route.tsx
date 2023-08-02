ae38fce95cf36a614f2921f45b1576b28e8d038e
Vladas <baradulinasvladas@gmail.com>
2023-08-02 12:58:16 +0300

    removed apllication/json

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -2,18 +2,14 @@ import { json } from "stream/consumers";
 import clientPromise from "../../lib/mongodb";
 
 export async function GET(req: Request) {
-  
-    const client = await clientPromise;
-    const db = client.db("Portfolio");
+  const client = await clientPromise;
+  const db = client.db("Portfolio");
 
-    const movies = await db.collection("Projects").find().toArray();
+  const movies = await db.collection("Projects").find().toArray();
 
-    const json = JSON.stringify(movies);
+  const json = JSON.stringify(movies);
 
-    const response = new Response(json, {
-      contentType: "application/json",
-    });
+  const response = new Response(json, {});
 
-    return response;
- 
+  return response;
 }
