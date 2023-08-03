12f097730e656964f55b45131a6ca80e4b314acd
Vladas <baradulinasvladas@gmail.com>
2023-08-03 11:39:58 +0300

    k

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,18 +1,15 @@
+import clientPromise from "../../lib/mongodb";
+import cors from "cors";
 
-// import clientPromise from "../../lib/mongodb";
-// import cors from "cors";
+export async function GET(req: Request) {
+  const client = await clientPromise;
+  const db = client.db("Portfolio");
 
+  const movies = await db.collection("Projects").find().toArray();
 
+  const json = JSON.stringify(movies);
 
-// export async function GET(req: Request) {
-//   const client = await clientPromise;
-//   const db = client.db("Portfolio");
+  const response = new Response(json, {});
 
-//   const movies = await db.collection("Projects").find().toArray();
-
-//   const json = JSON.stringify(movies);
-
-//   const response = new Response(json, {});
-
-//   return response;
-// }
+  return response;
+}
