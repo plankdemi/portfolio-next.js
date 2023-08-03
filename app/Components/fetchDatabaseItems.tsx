0f51cef160ba09c7dc3506630ce7179ef0f99c63
Vladas <baradulinasvladas@gmail.com>
2023-08-03 11:35:27 +0300

    removed api route

--- app/Components/fetchDatabaseItems.tsx
+++ app/Components/fetchDatabaseItems.tsx
@@ -1,26 +1,38 @@
 import AnotherContainer from "./anotherContainer";
 import axios from "axios";
+import clientPromise from "../../lib/mongodb";
 
-const API_ENDPOINT = "api";
+// const API_ENDPOINT = "api";
 
-const getAbsoluteURL = (relativeURL: String) => {
-  const vercelURL =
-    process.env.NEXT_PUBLIC_VERCEL_URL1 || process.env.NEXT_PUBLIC_VERCEL_URL2;
-  return `${vercelURL}${relativeURL}`;
-};
+// const getAbsoluteURL = (relativeURL: String) => {
+//   const vercelURL =
+//     process.env.NEXT_PUBLIC_VERCEL_URL1 || process.env.NEXT_PUBLIC_VERCEL_URL2;
+//   return `${vercelURL}${relativeURL}`;
+// };
 
 export default async function FetchDatabaseItems(props: any) {
-  const absoluteURL = getAbsoluteURL(API_ENDPOINT);
-  const response = await axios.get(absoluteURL);
+  // const absoluteURL = getAbsoluteURL(API_ENDPOINT);
+  // const response = await axios.get(absoluteURL);
 
-  if (response.status === 200) {
-    const projects = response.data;
-    const filteredProjects = projects.filter(
-      (project: any) => project.type === props.type
-    );
+  // if (response.status === 200) {
+  //   const projects = response.data;
+  //   const filteredProjects = projects.filter(
+  //     (project: any) => project.type === props.type
+  //   );
 
-    return <AnotherContainer projects={filteredProjects}></AnotherContainer>;
-  } else {
-    console.log("Error fetching projects");
-  }
+  //   return <AnotherContainer projects={filteredProjects}></AnotherContainer>;
+  // } else {
+  //   console.log("Error fetching projects");
+  // }.
+
+  const client = await clientPromise;
+  const db = client.db("Portfolio");
+
+  const projects = await db.collection("Projects").find().toArray();
+
+  const filteredProjects = projects.filter(
+    (project: any) => project.type === props.type
+  );
+
+  return <AnotherContainer projects={filteredProjects}></AnotherContainer>;
 }
--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,18 +1,18 @@
 
-import clientPromise from "../../lib/mongodb";
-import cors from "cors";
+// import clientPromise from "../../lib/mongodb";
+// import cors from "cors";
 
 
 
-export async function GET(req: Request) {
-  const client = await clientPromise;
-  const db = client.db("Portfolio");
+// export async function GET(req: Request) {
+//   const client = await clientPromise;
+//   const db = client.db("Portfolio");
 
-  const movies = await db.collection("Projects").find().toArray();
+//   const movies = await db.collection("Projects").find().toArray();
 
-  const json = JSON.stringify(movies);
+//   const json = JSON.stringify(movies);
 
-  const response = new Response(json, {});
+//   const response = new Response(json, {});
 
-  return response;
-}
+//   return response;
+// }
